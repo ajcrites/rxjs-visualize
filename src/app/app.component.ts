122617baefd91d0c5fdcda0179479db1064fde7b
Andrew Crites <acrites@mobiquityinc.com>
2019-01-17 17:11:48 -0500

    feat(frame): Adding general app frame and styles + some cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'rx-visualizations-app',
   template: `
+    <header class="header">
+      <img src="assets/rxjs-logo.png" />
+      <h1 class="page-title">RxJS Visualize</h1>
+    </header>
+    <aside class="small-screen-warning">
+      <strong>Warning:</strong> This app is intended to be viewed on a
+      relatively wide screen and is not responsive for smaller screens. It
+      should still work on smaller screens, but you may have to scroll or
+      otherwise manipulate your screen size to see the full visualizations.
+    </aside>
     <rx-visualizations-list></rx-visualizations-list>
   `,
 })

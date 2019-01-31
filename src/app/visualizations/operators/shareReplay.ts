6a3bc5abc3319a08cc489e94861107fc0aaff28b
Corey Pyle <cpyle@mobiquityinc.com>
2019-01-30 20:11:19 -0500

    feat(visualizations): Improving shareReplay visualization
    * Adding a new output marble
    * Removing comment about improvements being needed
    * Updating description to include new output names

--- src/app/visualizations/operators/shareReplay.ts
+++ src/app/visualizations/operators/shareReplay.ts
@@ -12,8 +12,8 @@ import { take, shareReplay, mergeMapTo } from 'rxjs/operators';
       subscriptions. One interesting note is that this replays from subscription
       to the *subject* Observable rather than the source Observable. If we
       didn't have a visualization for the subject (which creates a
-      subscription), <code>output</code> would emit the same as
-      <code>input</code> other than the initial delay.
+      subscription), <code>output1</code> and <code>output2</code> would emit
+      the same as <code>input</code> other than the initial delays.
     </p>
     <p>
       The replayed values are emitted all at once. The only difference from
@@ -22,22 +22,17 @@ import { take, shareReplay, mergeMapTo } from 'rxjs/operators';
       replay any values. <code>shareReplay()</code> will replay
       <em>all</em> values.
     </p>
-    <p>
-      This visualization doesn't really show off the
-      <code>share</code> capabilities and can probably be improved. See the
-      <a routerLink="/share"><code>share</code></a> visualization to see what I
-      mean.
-    </p>
     <pre prism-highlight="typescript">{{ code }}</pre>
 
     <marble [source]="input"></marble> <marble [source]="subject"></marble>
-    <marble [source]="output"></marble>
+    <marble [source]="output1"></marble> <marble [source]="output2"></marble>
   `,
 })
 export class RxShareReplayComponent {
   code = preval`module.exports = require('../codefile')(__filename)`;
 
-  input = timer(0, 1000).pipe(take(4));
+  input = timer(0, 1000).pipe(take(6));
   subject = this.input.pipe(shareReplay(2));
-  output = timer(3500).pipe(mergeMapTo(this.subject));
+  output1 = timer(2000).pipe(mergeMapTo(this.subject));
+  output2 = timer(4000).pipe(mergeMapTo(this.subject));
 }
